f14d258959641308b4e25229be6028f5d9b524bd
Melissa Bell <melissabell456@gmail.com>
2018-03-04 12:17:52 -0600

    completed flag exercise

---
+++ single-doc-exercises/flag.js
@@ -0,0 +1,20 @@
+const chalk = require('chalk');
+
+const star1 = chalk.white.bgBlue("   * * * * * * *    ");
+const star2 = chalk.white.bgBlue("    * * * * * *     ");
+const redLine = chalk.bgRed("          ");
+const whiteLine = chalk.bgWhite("          ");
+
+console.log(star1 + redLine + redLine + redLine);
+console.log(star2 + whiteLine + whiteLine + whiteLine);
+console.log(star1 + redLine + redLine + redLine);
+console.log(star2 + whiteLine + whiteLine + whiteLine);
+console.log(star1 + redLine + redLine + redLine);
+console.log(star2 + whiteLine + whiteLine + whiteLine);
+console.log(star1 + redLine + redLine + redLine);
+console.log(whiteLine + whiteLine + whiteLine + whiteLine + whiteLine);
+console.log(redLine + redLine + redLine + redLine + redLine);
+console.log(whiteLine + whiteLine + whiteLine + whiteLine + whiteLine);
+console.log(redLine + redLine + redLine + redLine + redLine);
+console.log(whiteLine + whiteLine + whiteLine + whiteLine + whiteLine);
+console.log(redLine + redLine + redLine + redLine + redLine);
